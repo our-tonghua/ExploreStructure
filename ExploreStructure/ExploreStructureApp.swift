edc7e3267ef23df1c1f3d27db31c11cd26d49c41
our-tonghua <t0n9hua@gmail.com>
2024-09-26 02:08:44 -0700

    Initial Commit

---
+++ ExploreStructure/ExploreStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreStructureApp.swift
+//  ExploreStructure
+//
+//  Created by Administrator on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ExploreStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
